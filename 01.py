ffb4a23c01647a5a2807e5030e351d6139d2a2e6
VladimirPalanzeev <palanzey@gmail.com>
2022-01-23 19:28:51 +0500

    [upd] Вывод координат кратчайшего пути

--- 01.py
+++ 01.py
@@ -51,6 +51,21 @@ def findPath(a, x, y, number):
     if a[x - 1][y] == 0:
         findPath(a, x - 1, y, number + 1)
 
+def getRoad(a, x, y):
+    res = []
+
+    res.append([x, y])
+    if a[x + 1][y] == a[x][y] - 1:
+        return res + getRoad(a, x + 1, y)
+    if a[x - 1][y] == a[x][y] - 1:
+        return res + getRoad(a, x - 1, y)
+    if a[x][y + 1] == a[x][y] - 1:
+        return res + getRoad(a, x, y + 1)
+    if a[x][y - 1] == a[x][y] - 1:
+        return res + getRoad(a, x, y - 1)
+
+    return res
+
 print("До обработки: ")
 printLab(a)
 print()
@@ -58,5 +73,9 @@ findPath(a, 1, 1, 1)
 print("После обработки")
 printLab(a)
 
+road = getRoad(a, 7, 9)
+road.reverse()
 
-
+print("Координаты пути: ")
+for i in road:
+    print(i)
